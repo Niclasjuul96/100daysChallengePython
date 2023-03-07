1aa85032cebcb5b2ad94ffcd9485567ffd72a4b8
Niclas Schæffer <Niclasschaeffer96@gmail.com>
2023-03-07 21:15:04 +0100

    Day5 and Day6 is completed!

---
+++ Day6/main.py
@@ -0,0 +1,38 @@
+print("Hello, world!")
+num_char = len("Hello, world!")
+
+print(num_char)
+
+#defining the Function
+def my_function():
+    print("Hello")
+    print("Bye")
+
+#Calling the Function
+my_function()
+
+
+def my_function2():
+    if 1==1:
+        print("Hello")
+    elif 1==2:
+        print("Hello")
+    elif 1==3:
+        print("Hello")
+    else:
+        print("World")
+
+
+#for item in list_of_items:
+    #Do something to each item
+
+
+#for item in range(1,11)
+    #do something 10 times
+
+#while something is true:
+    #do this.
+
+#infinite loop
+#while 5 > 3
+    #print("Hello")
